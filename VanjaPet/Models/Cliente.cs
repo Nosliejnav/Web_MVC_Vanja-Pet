2536e811ba9f1279057f790b65d867b84f66331f
Nosliejnav <vanjeilsonm10@gmail.com>
2024-05-16 20:38:44 -0300

    Criando paramentros

--- VanjaPet/Models/Cliente.cs
+++ VanjaPet/Models/Cliente.cs
@@ -1,6 +1,5 @@
 namespace VanjaPet.Models;
 
-
 public class Cliente
 {
     public int Id { get; set; } 
@@ -8,5 +7,14 @@ public class Cliente
     public string? Cpf { get; set; }
     public string? Email { get; set; }
     public string? Paciente { get; set; }
-    
+
+    public Cliente(int id, string nome, string cpf, string email, string paciente)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.Email = email;
+        this.Paciente = paciente;
+    }
+
 }
