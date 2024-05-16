b3f310fa1323ab4fce50c3a168f529bd4b6611ea
Nosliejnav <vanjeilsonm10@gmail.com>
2024-05-16 20:34:13 -0300

    Criando classe Cliente

---
+++ VanjaPet/Models/Cliente.cs
@@ -0,0 +1,12 @@
+namespace VanjaPet.Models;
+
+
+public class Cliente
+{
+    public int Id { get; set; } 
+    public string? Nome { get; set; }
+    public string? Cpf { get; set; }
+    public string? Email { get; set; }
+    public string? Paciente { get; set; }
+    
+}
